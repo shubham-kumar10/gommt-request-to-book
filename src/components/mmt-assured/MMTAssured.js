26a19c363c885cb6f78af1f0fafd8be1bdda0719
shubham.snu@gmail.com <shubham.snu@gmail.com>
2021-04-10 09:32:29 +0530

    feat: MMT Assured icon component

---
+++ src/components/mmt-assured/MMTAssured.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./mmt-assured.module.scss";
+
+function MMTAssured() {
+  return <div className="mmtAssuredIcon">MMT Assured</div>;
+}
+
+export default MMTAssured;
